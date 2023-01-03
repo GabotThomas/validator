d40012473c22204a9cbff98b141be9e9dc0ba23c
GeoDaz <gfrydz2@gmail.com>
2023-01-03 14:22:27 +0100

    Add collection patch

--- Validator/RecursiveContextualValidator.php
+++ Validator/RecursiveContextualValidator.php
@@ -333,6 +333,9 @@ class RecursiveContextualValidator implements ContextualValidatorInterface
      */
     private function validateEachObjectIn(iterable $collection, string $propertyPath, array $groups, ExecutionContextInterface $context)
     {
+        if (is_object($collection) && method_exists($collection, 'getValues')) {
+            $collection = $collection->getValues();
+        }
         foreach ($collection as $key => $value) {
             if (\is_array($value)) {
                 // Also traverse nested arrays
@@ -363,7 +366,7 @@ class RecursiveContextualValidator implements ContextualValidatorInterface
      * Validates a class node.
      *
      * A class node is a combination of an object with a {@link ClassMetadataInterface}
-     * instance. Each class node (conceptually) has zero or more succeeding
+     * instance. Each class node (conceptionally) has zero or more succeeding
      * property nodes:
      *
      *     (Article:class node)
